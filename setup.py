e858ccbde556bc1d06d5a8386bf8c4f0e7e22e6d
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2016-12-19 19:15:55 +0300

    APM-255 switching to npoed-massmail package

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='npoed-plp-extension-email',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     description='PLP extension email',
     url='http://example.com',
     author='author',
+    dependency_links=['https://github.com/npoed/npoed-massmail/tarball/master#egg=npoed_massmail-0.1'],
+    install_requires=["npoed_massmail==0.1"],
 )
 
